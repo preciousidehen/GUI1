10a09bf08d448c8552b2b13562cfc9d469bf1a8c
preciousidehen <48460830+preciousidehen@users.noreply.github.com>
2019-04-28 09:00:56 -0500

    Add files via upload

---
+++ Dialog1.java
@@ -0,0 +1,22 @@
+import javax.swing.JOptionPane;
+
+/**
+ * 
+ */
+
+/**
+ * @author Admin1
+ *
+ */
+public class Dialog1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JOptionPane.showMessageDialog(null, "welcome\nto\njava");
+
+	}
+
+}
