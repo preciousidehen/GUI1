19d18109e5a5c5b77acab7b3442696aa5b25ab66
preciousidehen <48460830+preciousidehen@users.noreply.github.com>
2019-05-04 00:02:13 -0500

    Add files via upload

--- Dialog1.java
+++ Dialog1.java
@@ -15,7 +15,9 @@ public class Dialog1 {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		JOptionPane.showMessageDialog(null, "welcome\nto\njava");
+		String name = JOptionPane.showInputDialog("What is your name");
+		String message = String.format("welcome %s to java", name);
+		JOptionPane.showMessageDialog(null, message);
 
 	}
 
